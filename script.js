5c70ac7f4809502e5325d1a3269b6600cad7b9ae
Ramkrishna Roy <76205677+Ramkrishna307@users.noreply.github.com>
2023-04-02 19:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-//your code here
-// script.js
 let bandNames = ['The Beatles', 'Led Zeppelin', 'The Who', 'Pink Floyd', 'Queen', 'The Rolling Stones', 'The Doors', 'The Jimi Hendrix Experience'];
 
 // Define a function to remove articles from a string
 function removeArticle(name) {
-  let words = name.split(' ');
-  if (words.length > 1 && (words[0].toLowerCase() === 'the' || words[0].toLowerCase() === 'a' || words[0].toLowerCase() === 'an')) {
-    return words.slice(1).join(' ');
-  } else {
-    return name;
-  }
+  let regex = /^(The|A|An)\s/i;
+  return name.replace(regex, '');
 }
 
 // Sort the band names without articles
 let sortedBandNames = bandNames.map(removeArticle).sort();
 
 // Display the sorted band names in an unordered list
-let ul = document.getElementById('bands');
-for (let i = 0; i < sortedBandNames.length; i++) {
+let ul = document.createElement('ul');
+ul.setAttribute('id', 'bands');
+
+sortedBandNames.forEach(function(bandName) {
   let li = document.createElement('li');
-  li.textContent = sortedBandNames[i];
+  li.textContent = bandName;
   ul.appendChild(li);
-}
+});
+
+document.body.appendChild(ul);
 
